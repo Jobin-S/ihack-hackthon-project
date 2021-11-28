75b3f61ca2c410e70a6dda56f4168862bccad66c
Aswin Asok <aswinasokofficial@gmail.com>
2021-11-28 13:39:52 +0530

    Landing page done, image not loading issue

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,92 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div class="nav-bar">
+        <div class="container_nav-bar">
+          <header>Wastified.</header>
+          <div id="login">
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="container">
+          <main>
+            <h1>
+              We Offer a Give and <br /> T&nbsp;ake Polic&nbsp;y.
+            </h1>
+          </main>
+
+          <div id="main-button">
+            <button id="give-button">
+              Give Waste
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="24"
+                viewBox="0 0 25 24"
+                fill="none"
+              >
+                <path
+                  d="M12.5 22C18.0228 22 22.5 17.5228 22.5 12C22.5 6.47715 18.0228 2 12.5 2C6.97715 2 2.5 6.47715 2.5 12C2.5 17.5228 6.97715 22 12.5 22Z"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8.5 12H16.5"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+            <button id="take-button">
+              Take Waste
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                  stroke="#84B454"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12 8V16"
+                  stroke="#84B454"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8 12H16"
+                  stroke="#84B454"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div id="main-image">
+            <img src="./Group3.png" alt="main-image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
